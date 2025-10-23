4023b54360900e7bd60c7d5489115e18deb8c89d
Nathaly <otidg15@igp.gob.pe>
2025-10-23 17:38:21 -0500

    Feat: uso de const sobre let y var

---
+++ src/bases/01-const-let.js
@@ -0,0 +1,18 @@
+const firstName1 = "Tony";
+const lastName1 = "Stark";
+
+const fullname1= `${firstName} ${lastName}`
+
+
+const firstName2 = "Lets";
+const lastName2 = "Go";
+
+if (true) {
+  const firstName = "Peter";
+}
+
+console.log({firstName1, lastName1});
+
+console.log(fullname1);
+
+console.log({firstName2, lastName2});
